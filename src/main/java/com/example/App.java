3e9eb5ef3c1452a4c83deb483f8235a76ddeeb5f
Harishebsco <167107980+Harishebsco@users.noreply.github.com>
2025-09-21 08:48:00 -0400

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,5 +1,10 @@
+package com.example;
 public class App {
    public static void main(String[] args) {
-       System.out.println("Hello World!");
+       System.out.println("Hello from Maven + Jenkins!");
+   }
+   // simple method for testing
+   public static String greet() {
+       return "Hello Test";
    }
 }
