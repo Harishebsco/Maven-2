8eefdc4afc57a6c047ed23af8a826eab035702a9
Harishebsco <167107980+Harishebsco@users.noreply.github.com>
2025-09-21 08:48:32 -0400

    Update AppTest.java

--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -1,9 +1,9 @@
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+package com.example;
+import org.junit.Test;
+import static org.junit.Assert.*;
 public class AppTest {
    @Test
-   void testMessage() {
-       String message = "Hello World!";
-       assertEquals("Hello World!", message);
+   public void testGreet() {
+       assertEquals("Hello Test", App.greet());
    }
 }
